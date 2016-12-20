06bd1feb69e37fedf6006e5ac87e62a977d844c8
AlekVT <alek.vtichelen@gmail.com>
2016-12-20 17:45:07 +0100

    cleaned up controllers, most things now work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,131 +1,4 @@
-let loggedIn = false;
-let url = 'https://gingerwald.com/community/v2.1/';
-/* placeholder token for developement */
-let token = 'CvBFRdXboqHC8eCOm0ONXRwtBsIWIpgYI5QZ0BsKQzHHSc3PCkg3E4su4J8P3vPa';
-let client_id = 'GingerwaldUserApp11';
-let client_secret = 'HvU5T8VcUBMV1rmjOPsLNSQ3hjsAolNQXu7kVOikJWX7vdiQpXca7UqVevZPgh8E';
-let drink;
-let userCredits;
-let userKey;
-
 angular.module('app')
-
-.factory('menuSrv', function($ionicSideMenuDelegate, $http) {
-    return {
-        login : function(email, password) {
-           return $http.post(url.concat('/authorization/oauth/token.php?grant_type=password&username=', email, '&password=', password, '&client_id=', client_id, '&client_secret=', client_secret));
-           /* return $.ajax({
-                type: 'POST',
-                url: url.concat('/authorization/oauth/token.php?grant_type="password"&username=', email, '&password=', password, '&client_id=', client_id, '&client_secret=', client_secret),
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function() {
-                    console.log('things went wrong');
-                }
-            });*/
-        },
-        logout : function() {
-            loggedIn = false;
-            window.location.replace('');
-        },
-        
-        fetchUserData : function() {
-            return $.ajax({
-                type: 'GET',
-                url: 'https://gingerwald.com/community/v2.1/api/getUserDetails.php?token=CvBFRdXboqHC8eCOm0ONXRwtBsIWIpgYI5QZ0BsKQzHHSc3PCkg3E4su4J8P3vPa',
-                dataType: 'json',
-                success: function(data) {
-                    userCredits = data.User.NumberCredits;
-                    userKey = data.User.Login;
-                },
-                error: function(err) {
-                    console.log('userdata not available: ' + err);
-                }
-            });
-        },
-        
-        getUserData : function() {
-            return {credits: userCredits, key: userKey};
-        },
-
-        redirect : function(location) {
-            window.location.replace('#/' + location);
-        },
-
-        toggleMenu : function() {
-            $ionicSideMenuDelegate.toggleLeft();
-        }
-    };
-})
-
-.factory('scanSrv', function($http) {
-    return {
-        scanDrink : function(string) {
-            /* disabled for developement 
-            return $http.jsonp(url + 'api/getJuiceDetails.php?token=' + token + '&bottle_token=' + string);
-            developement code *
-            return { 
-                Juice: {
-                    ID: 16,
-                    Name: "Cucumber Mojito",
-                    CatalogNumber: 16,
-                    Description: "OK, it's not the real Cuban mojito, but this is what Hemingway probably would have invented if he had a daytime job where no(t too much) alcohol was allowed.\nGo south of the border with this juice, and enjoy the easy nutrients and vitamins coming your way.",
-                    PictureName: "cucumber-grape-lime-mint.png",
-                    Amount_ml: 105
-                }
-            };*/
-            return $.ajax({
-                type: 'GET',
-                url: url.concat('api/getBottleDetails.php?token=', token, '&bottle_token=', string),
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function() {
-                    console.log('nope');
-                }
-            });
-        },
-        
-        addToDash : function(bottle_token) {
-            return $.ajax({
-                type: 'POST',
-                url: url.concat('api/addBottleToDashboard.php?token=', token, '&bottle_token=', bottle_token),
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function() {
-                    console.log('nope');
-                }
-            });
-        }
-    };
-})
-
-.factory('dashboardSrv', function() {
-    return {
-        getUserDash : function(from, to) {
-            var dashUrl = url.concat('api/getUserDashboard.php?token=', token);
-            if (from !== null && to !== null) {
-                dashUrl.concat('&report_from=', from, '&report_to=', to);
-            }
-            return $.ajax({
-                type: 'GET',
-                url: dashUrl,
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function() {
-                    console.log('nope');
-                }
-            });
-        }
-    };
-})
 
 .controller('sideMenuCtrl', function($scope, menuSrv) {
     $scope.redirect = function(location) {
@@ -138,20 +11,21 @@ angular.module('app')
     };
 })
 
-.controller('loginCtrl', function($scope, $http, $location, menuSrv) {
+.controller('loginCtrl', function($scope, $location, loginSrv, menuSrv) {
 	$scope.login = function() {
-		let email = 'plantijn002@gingerwald.be' // document.getElementById('email').value; // plantijn002@gingerwald.be
-		let password = 'gingerjuice' // document.getElementById('password').value; // gingerjuice
-        menuSrv.fetchUserData()
-            .then( 
-            function() {
-                window.location.replace('#/menu');
+		let email = document.getElementById('email').value; // plantijn002@gingerwald.be
+		let password = document.getElementById('password').value; // gingerjuice
+        loginSrv.login()
+        .then(function() {
+            menuSrv.fetchUserData()
+            .then(function() {
+                if (drink != null) {
+                    window.location.replace('#/bottleDetails');
+                } else {
+                    window.location.replace('#/menu');
+                }
             });
-        /*
-        menuSrv.login(email, password).then(function(data) {
-            loggedIn = true;
-            $location.url('menu');
-        });*/
+        });
 	};
     
     $scope.scan = function() {
@@ -177,7 +51,7 @@ angular.module('app')
     };
 })
 
-.controller('scanCtrl', function($scope, $cordovaBarcodeScanner, scanSrv) {
+.controller('scanCtrl', function($scope, scanSrv) {
     $scope.cancel = function() {
         window.location.replace('#/menu');
     };
@@ -188,19 +62,13 @@ angular.module('app')
     http://qr.gingerwald.com?b=3DJ3DnJxtSem6W
     **/
     $scope.scanBarcode = function() {
-        /* disabled for developement 
-        console.log("attempting scan");
-        $cordovaBarcodeScanner.scan().then(function(imageData) {
-            alert(imageData.text);
-            console.log("format: " + imageData.format);
-            console.log("cancelled: " + imageData.cancelled);
-        }, function(error) {
-            alert("an error occured: " + error);
+        scanSrv.scanBottle().then(function() {
+            scanSrv.fetchDrink().then(function() {
+                scanSrv.getDrink().then(function() {
+                    window.location.replace('#/bottleDetails');
+                });
+            });
         });
-        */
-        /* execute on success  // should respond on promise */
-        drink = scanSrv.scanDrink('Lh3UGloz6ya624');
-        window.location.replace('#/bottleDetails');
     };
     /* disabled for developement
     var permissions = cordova.plugins.permissions;
@@ -227,9 +95,11 @@ angular.module('app')
     $scope.loggedIn = loggedIn;
     
     $scope.addToDash = function() {
-        /* disabled for developement
-        scanSrv.addToDash('Lh3UGloz6ya624'); */
-        window.location.replace('#/dashboard');
+        /* disabled for developement */
+        scanSrv.addToDash().then(function() {
+            window.location.replace('#/dashboard');
+        });
+        //window.location.replace('#/dashboard');
     };
     
     $scope.cancel = function() {
@@ -237,9 +107,7 @@ angular.module('app')
     };
     
     $scope.login = function() {
-        /* login logic */
-        loggedIn = $scope.loggedIn = true;
-        window.location.replace('#/bottleDetails');
+        window.location.replace('#/login');
     }
 })
 
